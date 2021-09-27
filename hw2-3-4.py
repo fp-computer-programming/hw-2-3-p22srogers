b8bdb8c358e72b4bbe64e4f6fd879b282ffea16b
p22srogers <90051572+p22srogers@users.noreply.github.com>
2021-09-26 20:51:48 -0400

    Update hw2-3-4.py

--- hw2-3-4.py
+++ hw2-3-4.py
@@ -1,2 +1,8 @@
 # Author: SMR (AMDG) 09/23/21
-print("The expected population on September 24th 2021 is 332778054")
+population_now=331832432
+minute=60/25
+hour=min*60
+day=hour*24
+year=day*365
+prediction=year+population_now
+print(prediction)
